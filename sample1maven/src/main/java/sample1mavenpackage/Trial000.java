5e334d16448d8c64e24c46959b1db61d1e8f0dea
hp <hp@DESKTOP-TVSUR7N>
2024-11-14 21:59:50 -0800

    second commit

---
+++ sample1maven/src/main/java/sample1mavenpackage/Trial000.java
@@ -0,0 +1,11 @@
+package sample1mavenpackage;
+
+public class Trial000 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My name is Vishnu");
+
+	}
+
+}
